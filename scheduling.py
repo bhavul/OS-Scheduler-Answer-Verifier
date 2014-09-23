4905f32e4caa01957ad3a274248cf5b6ac87b10b
Bhavul Gauri <bhavul93@gmail.com>
2014-09-23 21:20:02 +0530

    flushed the sjf code. Started writing the algo...

--- scheduling.py
+++ scheduling.py
@@ -41,43 +41,18 @@ def fcfs():
 def sjf():
 	"""
 	Shortest job first
-	bug : you can't provide same arrival times for two processes. They have to differ by a second at least! cuz keys can't be same.
-
+	
 	here, you'll first give priority to time and then the cpu burst time. See, how many processes have really arrived. 
 	If something has arrived, then check it's cpu burst time.
 
-	> make a list of processes that are there at t.
-	> sort them according to their cpu_time.
-	> print and stuff.
+	need : 3 original arrays, 1 rdy queue, tempExecEnd, tempExecEnd2, arrayForIndices
+
+	algo : 
+	> Put the arrival times and cpu burst times and pids in three arrays with corresponding indexes - call 'em original arrays
+	> Find the smallest arrival time. Check how many processes are coming then.
+	
 	"""
-	dicto = {}	#start time will be the key.
-	current = {}
-	for i in range(0,len(start_time)):
-		dicto[int(start_time[i])] = [p_id[i],cpu_time[i]]
-	length = len(dicto)
-	t = 0 #time
-	w = 0	#for average waiting time.	
-	for key in sorted(dicto):
-		current = {}
-		if(int(key) <= int(t)):
-			current[int(key)] = dicto[key]
-
-	for key1 in sorted(current):
-		a = current[key1]
-		w += t
-		print "from t = %-5s to t = %-5s => P%-5s	" % (int(t),int(t)+int(a[1]),int(a[0]))
-		t += int(a[1])
-		del dicto[int(key1)]		
-		break
-
-		"""
-		a = dict[key]	#gets an array of pid (0th index) and start_time (1st index).
-		w += t;
-		print "from t = %-5s to t = %-5s => P%-5s   " % (int(t),int(t)+int(key),int(a[0]))
-		t += int(key)
-		"""
-	w = w/float(length)
-	print "\n Average waiting time : %-2.2f" % (w)
+	
 
 
 
